cea9d9f4ad4bdf43c4a24b8c4793dd8c1b174fa3
Preston Lamb <plamb@axis41.com>
2015-07-30 13:28:17 -0600

    changed the query param for the Post so you can get a post by the slug

--- app/js/factories/Post.js
+++ app/js/factories/Post.js
@@ -1,5 +1,5 @@
 angular.module('myApp').factory('Post', ['$resource', function($resource){
-	var PostResource = $resource('/wordpress/wp-json/posts/:id', {_id: "@id"});
+	var PostResource = $resource('/wordpress/wp-json/posts/?slug=:slug');
 
 	return PostResource;
 }])
